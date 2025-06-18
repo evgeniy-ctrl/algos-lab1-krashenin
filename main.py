c163c79b625e802f8b070f7db1861b9dbd91939d
evgeniy-ctrl <oneeperceentt@gmail.com>
2025-06-18 13:40:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,16 @@ hint: что такое print?
 hint: цикл, если и "%"
 """
 
-# Замените это своим кодом
+x = int(input("Введите число")) + 1 
+for i in range(1,x):
+  if i % 3 == 0 and i % 5 == 0:
+    print("FizzBuzz")
+  elif i % 3 == 0:
+    print("Fizz")
+  elif i % 5 == 0:
+    print("Buzz")
+  else:
+    print(i)
 
 """
  _____         _      _____ 
