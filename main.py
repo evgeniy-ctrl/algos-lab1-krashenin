acd0bde5e8abaf066ba939b9ba9d01347433fe73
evgeniy-ctrl <oneeperceentt@gmail.com>
2025-06-18 13:50:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -51,8 +51,11 @@ for i in range(1,x):
 hint: https://ru.wikihow.com/%D0%B2%D1%8B%D1%81%D1%87%D0%B8%D1%82%D1%8B%D0%B2%D0%B0%D1%82%D1%8C-%D0%B2%D0%B8%D1%81%D0%BE%D0%BA%D0%BE%D1%81%D0%BD%D1%8B%D0%B5-%D0%B3%D0%BE%D0%B4%D1%8B
 """
 
-# Замените это своим кодом
-
+x = int(input("Введите год: "))
+if x % 4 == 0:
+    print('Високосный год')
+elif x % 4 != 0:
+    print('Не високосный год')
 """
  _____         _        ___ 
 |_   _|       | |      /   |
